6460e19f73a89b5d12057edbf2471c04027c572c
Peregud Oxana <ox.peregud@gmail.com>
2022-03-24 12:56:59 +0300

    feat: added expense item styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <div>
-                    <ExpenseItem/>
-                </div>
-            </header>
-        </div>
+        <ExpenseItem/>
     );
 }
 
