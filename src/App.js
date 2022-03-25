884456186cb9c943d70d68a8526aaa22a8d6181d
Peregud Oxana <ox.peregud@gmail.com>
2022-03-25 14:18:38 +0300

    feat: moved data from expense form (child) to new expense (parent)

--- src/App.js
+++ src/App.js
@@ -32,9 +32,14 @@ const App = () => {
         },
     ];
 
+    const addExpenseHandler = (expense) => {
+        expenses.push(expense);
+        console.log(expenses);
+    }
+
     return (
         <div>
-            <NewExpense/>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}/>
         </div>
     );
