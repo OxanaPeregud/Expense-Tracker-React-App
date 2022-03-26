c21fdb06ea598f7b24314414ba71688d34ddba4b
Peregud Oxana <ox.peregud@gmail.com>
2022-03-26 11:13:08 +0300

    feat: added expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
 
@@ -16,25 +16,12 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense =>
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        );
-    }
-
     return (
         <Card className={"expenses"}>
             <ExpensesFilter
                 selectedYear={filteredYear}
                 onChangeFilter={filterChangeHandler}/>
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     );
 };
